4c1b8b7eab3c6bfb58174dedb437ae14781bd15e
Rohandalal8 <rohandalal079@gmail.com>
2025-03-12 14:59:07 +0530

    Style Routes

--- models/listing.js
+++ models/listing.js
@@ -23,13 +23,17 @@ const listingSchema = new mongoose.Schema({
     image: {
         filename: {
             type: String,
-            default: 'listingimage'
+            default: 'defaultimage'
         },
         url: {
             type: String,
-            default: 'https://via.placeholder.com/150',
-            set: (v) => v === '' ? 'https://via.placeholder.com/150' : v
-        }
+            default:
+                "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            set: (v) =>
+                v === ""
+                    ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                    : v,
+        },
     },
     price: Number,
     location: String,
