e39eb9a3e6e96db4d021ee38e55b6f7ef62f80c3
Rohandalal8 <rohandalal079@gmail.com>
2025-03-29 12:30:43 +0530

    Completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/expressError.js');
 const session = require('express-session');
+const mongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -23,8 +24,21 @@ app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 app.use(express.static('public'));
 
+const store = mongoStore.create({
+    mongoUrl: process.env.ATLASDB_URL,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on('error', function (e) {
+    console.log('Session Store Error', e);
+});
+
 const sessionOptions = {
-    secret: 'thisisnotagoodsecret',
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
