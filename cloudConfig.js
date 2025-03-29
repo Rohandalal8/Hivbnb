52e472bbb14ce0045c9332252a4e1e7d4f023b13
Rohandalal8 <rohandalal079@gmail.com>
2025-03-29 13:41:32 +0530

    done

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'hivbnb_DEV',
-        allowed_formats: ['jpg', 'png', 'jpeg'],
+        allowed_formats: ['jpg', 'png', 'jpeg', 'avif'],
     },
 });
 
